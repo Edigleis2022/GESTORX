1a3618eb410715e6458aa04349d7f2c0c4e6eabe
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2026-03-10 20:59:13 -0300

    🚧 Código em Desenvolvimento 10/03/2026

--- src/main/java/br/ifms/edu/GetorX/repository/FornecedorRepository.java
+++ src/main/java/br/ifms/edu/GetorX/repository/FornecedorRepository.java
@@ -1,11 +1,8 @@
 package br.ifms.edu.GetorX.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import br.ifms.edu.GetorX.model.Fornecedor;
 
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
-
-    public Fornecedor save(Fornecedor fornecedor);
-
-    public Object findAll();
 
 }
--- src/main/java/br/ifms/edu/GetorX/repository/ProdutoRepository.java
+++ src/main/java/br/ifms/edu/GetorX/repository/ProdutoRepository.java
@@ -1,11 +1,8 @@
 package br.ifms.edu.GetorX.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import br.ifms.edu.GetorX.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
-    public Object findAll();
-
-    public Produto save(Produto produto);
 
 }
