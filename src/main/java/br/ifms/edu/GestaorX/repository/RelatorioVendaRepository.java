fdf1166b99de54a2f13a90edf3cc239649e3a2ce
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2026-03-15 07:44:33 -0400

    ⚡️ Novo Código adicionado 15/03/2026 07:00

--- src/main/java/br/ifms/edu/GestaorX/repository/RelatorioVendaRepository.java
+++ src/main/java/br/ifms/edu/GestaorX/repository/RelatorioVendaRepository.java
@@ -3,7 +3,9 @@ package br.ifms.edu.GestaorX.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.ifms.edu.GestaorX.model.RelatorioVenda;
+
 @Repository
-public interface RelatorioVendaRepository extends JpaRepository<RelatorioVendaRepository, Long>{
+public interface RelatorioVendaRepository extends JpaRepository<RelatorioVenda, Long>{
 
 }
