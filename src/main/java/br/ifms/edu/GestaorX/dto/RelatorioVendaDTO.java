7d433f48bcbc32044723a4a0e173d1c558dc2c1c
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2026-03-15 08:25:09 -0400

    ⚡️ Nova Classees Criada e annotation adicionada 15/03/2026 08:25

---
+++ src/main/java/br/ifms/edu/GestaorX/dto/RelatorioVendaDTO.java
@@ -0,0 +1,12 @@
+package br.ifms.edu.GestaorX.dto;
+
+import java.time.LocalDate;
+
+
+public class RelatorioVendaDTO {
+
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private Integer totalVendas;
+
+}
--- src/main/java/br/ifms/edu/GestaorX/dto/VendaDTO.java
+++ src/main/java/br/ifms/edu/GestaorX/dto/VendaDTO.java
@@ -1,7 +1,9 @@
 package br.ifms.edu.GestaorX.dto;
 
 import java.time.LocalDate;
+import lombok.Data;
 
+@Data
 public class VendaDTO {
 
     private Long id;
