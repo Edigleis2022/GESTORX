76a33cc3c1b34123a3b811fd9b73915dc31c1cc8
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2026-04-24 18:11:54 -0400

    ⚡️ Nova implementação ded segurança 24/04 18:21

--- src/main/java/br/ifms/edu/GestorX/securityConfig/SecurityConfig.java
+++ src/main/java/br/ifms/edu/GestorX/securityConfig/SecurityConfig.java
@@ -2,8 +2,10 @@ package br.ifms.edu.GestorX.securityConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,4 +15,14 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**", "/usuarios/**").permitAll() // 🔥 libera login e cadastro
+                .anyRequest().authenticated()
+            )
+            .build();
+    }
 }
